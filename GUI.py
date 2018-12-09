10c31198a7459e3ce99d7fa2bca328827409d446
BG6WRI <bg6wri@gmail.com>
2018-12-10 03:00:03 +0800

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -31,8 +31,10 @@ def start():
 		if sys.platform == "linux2":
 			ser=serial.Serial("/dev/ttyUSB"+e5.get(),9600,timeout=0.5)
 
-	print "You are tracking : "*i+u"正在追踪: "*(not i)+str.upper(Sat)+"."
-	print "You are at Lat : "*i+u"旋转器纬度: "*(not i)+str(Lat)+" Lon : "*i+u" 经度: "*(not i)+str(Lon)+" Altitude :"*i+u" 高度: "*(not i)+str(Alt)+"m"
+	print "You are tracking : "*i	+u"正在追踪: "*(not i)+str.upper(Sat)+"."
+	print "You are at Lat : "*i		+u"旋转器纬度: "*(not i)+str(Lat)
+				+" Lon : "*i		+u" 经度: "*(not i)+str(Lon)
+				+" Altitude :"*i	+u" 高度: "*(not i)+str(Alt)+"m"
 
 	line1,line2,Lat,Lon,Alt = GetUserData.get_user_data("gui",Sat,Lat,Lon,Alt)
 	GetSat.generate(line1,line2)
@@ -43,13 +45,13 @@ def start():
 	AZ,EL = GetLook.GetLook(tt,eciSat)
 	
 	if EL>=5:
-		print "Passing Now ..."*i+u"正在过境 ..."*(not i)
-		var.set("Passing Now ..."*i+u"正在过境 ..."*(not i))
+		print "Passing Now ..."*i		+u"正在过境 ..."*(not i)
+		var.set("Passing Now ..."*i		+u"正在过境 ..."*(not i))
 
 	if EL<5:
 		pass_time = GetLook.GetPassTime(tt,eciSat)
 		local_time = time.localtime(pass_time)
-		print "Next Passing Time:"*i+u"过境时间:"*(not i)+time.asctime(local_time)
+		print "Next Passing Time:"*i	+u"过境时间:"*(not i) + time.asctime(local_time)
 		var.set(time.asctime(local_time))
 
 
@@ -81,7 +83,7 @@ def fun_timer():
 	
 	#Serial Output
 	if mode.get() == 2 or mode.get() ==3 :
-		serial_str="AZ"+str(AZ)+" EL"+str(EL)+"Easycomm Mode"
+		serial_str="AZ"+str(AZ)+" EL"+str(EL)+" Easycomm"
 		ser.write(serial_str)
 
 	if stop == 0:
@@ -101,9 +103,9 @@ def root_gui():
 
 
 	#Button
-	ButtonStart = Button(	root, text="Start"*i+u"启动"*(not i),			command=start)
-	ButtonStop = Button(	root, text="Stop"*i+u"停止"*(not i),				command=stop)
-	ButtonUpdate = Button(	root, text="Update Data"*i+u"更新星历"*(not i),	command=update)
+	ButtonStart = Button(	root, text="Start"*i		+u"启动"*(not i),		command=start)
+	ButtonStop = Button(	root, text="Stop"*i			+u"停止"*(not i),		command=stop)
+	ButtonUpdate = Button(	root, text="Update Data"*i	+u"更新星历"*(not i),	command=update)
 	#ButtonUpdate = Button(	root, text="Update Data",	command=lambda:update)
 
 
@@ -112,11 +114,11 @@ def root_gui():
 	ButtonUpdate.grid(  row=0, column=2, padx=20, pady=5)
 
 	#UserData
-	Label(root, text="Sat Name:"*i+u"目标卫星"*(not i)).grid(	row=1)
-	Label(root, text="Lat:"*i+u"纬度"*(not i)).grid(				row=2)
-	Label(root, text="Lon:"*i+u"经度"*(not i)).grid(				row=3)
-	Label(root, text="Alt:"*i+u"高度"*(not i)).grid(				row=4)
-	Label(root, text="Serial COM"*i+u"串口号"*(not i)).grid(		row=4, column=2)
+	Label(root, text="Sat Name:"*i		+u"目标卫星"*(not i)).	grid(row=1)
+	Label(root, text="Lat:"*i			+u"纬度"*(not i)).		grid(row=2)
+	Label(root, text="Lon:"*i			+u"经度"*(not i)).		grid(row=3)
+	Label(root, text="Alt:"*i			+u"高度"*(not i)).		grid(row=4)
+	Label(root, text="Serial COM"*i		+u"串口号"*(not i)).		grid(row=4, column=2)
 
 	global e1,e2,e3,e4,e5
 
@@ -126,18 +128,16 @@ def root_gui():
 	e4 = Entry(root, width=10)
 	e5 = Entry(root, width=10)
 
-
-#	e1.insert(0,"SO-50")
-#	e2.insert(0,"30")
-#	e3.insert(0,"30")
-#	e4.insert(0,"0")
-
 	e1.grid(row=1, column=1, padx=20, pady=5)
 	e2.grid(row=2, column=1, padx=20, pady=5)
 	e3.grid(row=3, column=1, padx=20, pady=5)
 	e4.grid(row=4, column=1, padx=20, pady=5)
 	e5.grid(row=5, column=2, padx=20, pady=5)
 
+#	e1.insert(0,"SO-50")
+#	e2.insert(0,"30")
+#	e3.insert(0,"30")
+#	e4.insert(0,"0")
 
 	#Passing Situation
 
@@ -149,8 +149,8 @@ def root_gui():
 
 
 	#Joke
-	Label(root, text="Design by BG6WRI").grid(		row=7, column=2)
-	Label(root, text="From WITARC").grid(	row=8, column=2)
+	Label(root, text="Author : BG6WRI").	grid(row=7, column=2)
+	Label(root, text="From WITARC").		grid(row=8, column=2)
 	#ButtonJoke = Button(root, text="一键日卫星", command=lambda:var.set("和我没关系啊，你找OpenATS去"))
 	#ButtonJoke.grid(row=8, column=2)
 	#var = StringVar()
@@ -160,10 +160,12 @@ def root_gui():
 	#Output
 
 	#Scereen Ready
-	Label(root,text="AZ"*i+"方位角"*(not i)).grid(row=5,column=0)
-	Label(root,text="EL"*i+"仰角"*(not i)).grid(row=5,column=1)
+	Label(root,text="AZ"*i		+"方位角"*(not i)).	grid(row=5,column=0)
+	Label(root,text="EL"*i		+"仰角"*(not i)).	grid(row=5,column=1)
+
 	global AZ_flash
 	global EL_flash
+	
 	AZ_flash=IntVar()
 	AZ_flash.set("")
 	EL_flash=IntVar()
@@ -172,7 +174,7 @@ def root_gui():
 	Entry(root, textvariable=EL_flash, width=10).grid(row=6, column=1)
 
 	#Mode Choose
-	group = LabelFrame(root, text="output Mode"*i+u"输出模式"*(not i), padx=5, pady=5)
+	group = LabelFrame(root, text="output Mode"*i +u"输出模式"*(not i), padx=5, pady=5)
 	group.grid(row=1, column=2, padx=10, pady=10, rowspan=3)
 	LANGS = [("Screen"*i+u"屏幕"*(not i), 1), ("Serial"*i+u"串口"*(not i), 2),("Both"*i+u"俩都"*(not i), 3)]
 	global mode
