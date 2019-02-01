5346e39887ee09ba3c2b67e28ed92d16fe5ca21d
BG6WRI <bg6wri@gmail.com>
2019-02-02 06:10:46 +0800

    Update server.py

--- server.py
+++ server.py
@@ -11,53 +11,99 @@ import serial
 import GetUserData
 import GetSat
 import GetLook
+from Pelco_D import Tracker
 
 app = Flask(__name__)
 
 
-@app.route('/track', methods=['GET'])
+@app.route('/', methods=['GET'])
 def index():
-    context = {
-        'lat': 0,
-        'lon': 0,
-        'alt': 0,
-        'az': 0,
-        'el': 0
-    }
-    return render_template('track.html', **context)
+	context = {
+		'lat': 0,
+		'lon': 0,
+		'alt': 0,
+		'az': 0,
+		'el': 0
+	}
+	return render_template('index.html', **context)
+
+@app.route('/update', methods=['POST', "GET"])
+def update():
+	GetUserData.update("gui")
+	res = {
+		'az': "Update Done",
+		'el': "	",
+	}
+	return jsonify(res)
 
 
-@app.route('/tracker', methods=['POST', "GET"])
+@app.route('/predict', methods=['POST', "GET"])
+def predict():
+	receive = request.json
+
+	Sat = str(receive['satname'])
+	Lat = float(receive['lat'])
+	Lon = float(receive['lon'])
+	Alt = float(receive['alt'])
+
+	line1, line2, Lat, Lon, Alt = GetUserData.get_user_data("gui", Sat, Lat, Lon, Alt)
+	GetSat.generate(line1, line2)
+	GetLook.generate(Lat, Lon, Alt)
+
+	tt = time.time()
+	eciSat = GetSat.get_eciSat(tt)
+	AZ, EL = GetLook.GetLook(tt, eciSat)
+
+	res = {
+		'az': AZ,
+		'el': EL
+	}
+	return jsonify(res)
+
+@app.route('/newTracker', methods=['POST', "GET"])
+def newTracker():
+	global Tracker
+	Tracker = Tracker("/dev/ttyUSB0",2400)
+
+		
+
+	#return jsonify(res)
+
+
+@app.route('/step', methods=['POST', "GET"])
+def step():
+	#global Tracker
+	receive = request.json
+	if receive['cmd'] == "LD":
+		Tracker.down()
+		Tracker.left()
+	if receive['cmd'] != "LD":
+		if receive['cmd'] == "up":
+			Tracker.up()
+		if receive['cmd'] == "down":
+			Tracker.down()
+		if receive['cmd'] == "left":
+			Tracker.left()
+		if receive['cmd'] == "right":
+			Tracker.right()
+		time.delay(0.5)
+		Tracker.stop()
+		
+
+	#return jsonify(res)
+
+@app.route('/track', methods=['POST', "GET"])
 def track():
-    receive = request.json
-    if receive['lat'] == "update":
-        GetUserData.update("gui")
-        res = {
-            'az': "Update Done",
-            'el': "	",
-        }
-    else:
-        receive = request.json
-        Sat = str(receive['satname'])
-        Lat = float(receive['lat'])
-        Lon = float(receive['lon'])
-        Alt = float(receive['alt'])
+	receive = request.json
+	if receive['lat'] == "update":
+		GetUserData.update("gui")
+		res = {
+			'az': "Update Done",
+			'el': "	",
+		}
 
-        line1, line2, Lat, Lon, Alt = GetUserData.get_user_data("gui", Sat, Lat, Lon, Alt)
-        GetSat.generate(line1, line2)
-        GetLook.generate(Lat, Lon, Alt)
-
-        tt = time.time()
-        eciSat = GetSat.get_eciSat(tt)
-        AZ, EL = GetLook.GetLook(tt, eciSat)
-
-        res = {
-            'az': AZ,
-            'el': EL
-        }
-    return jsonify(res)
-
+	return jsonify(res)
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run('0.0.0.0', 8080)
+	app.debug = True
+	app.run('0.0.0.0', 8080)
